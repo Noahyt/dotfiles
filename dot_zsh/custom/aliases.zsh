e497a95ba9960fa0eafabec597c1430c416fa567
Noah <Noah.Toyonaga@gmail.com>
2022-04-11 15:44:22 -0400

    Add custom aliases

---
+++ dot_zsh/custom/aliases.zsh
@@ -0,0 +1,15 @@
+# ALIASES
+alias cm="chezmoi" 
+
+# SHORTCUTS
+export HRC_DATA="/n/holyscratch01/mahadevan_lab/ntoyonaga"
+export EDITOR="vim"
+
+# FUNCTIONS
+hrc_sync () {
+	SOURCE_FOLDER=${1}
+	DESTINATION_FOLDER=${2}
+	echo "sync ${SOURCE_FOLDER} with ${DESTINATION_FOLDER}"
+	rsync -avP --update ntoyonaga@login.rc.fas.harvard.edu:/n/holyscratch01/mahadevan_lab/ntoyonaga/${SOURCE_FOLDER} ${DESTINATION_FOLDER}
+}
+
