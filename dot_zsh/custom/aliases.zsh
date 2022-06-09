ed5e819b7571c018720c1d3fe563199cf504f764
Noah <Noah.Toyonaga@gmail.com>
2022-06-09 12:54:56 -0400

    move shortcuts to ~/custom/aliases.zsh

--- dot_zsh/custom/aliases.zsh
+++ dot_zsh/custom/aliases.zsh
@@ -6,6 +6,9 @@ export HRC_DATA="/n/holyscratch01/mahadevan_lab/ntoyonaga"
 export EDITOR="vim"
 export CIS_DATA="/Users/quinoah/Documents/data"
 
+export FABRIC="/Users/quinoah/Documents/FABRIC"
+
+
 # FUNCTIONS
 hrc_sync () {
 	SOURCE_FOLDER=${1}
@@ -14,3 +17,22 @@ hrc_sync () {
 	rsync -avP --update ntoyonaga@login.rc.fas.harvard.edu:/n/holyscratch01/mahadevan_lab/ntoyonaga/${SOURCE_FOLDER} ${DESTINATION_FOLDER}
 }
 
+# DOCUMENTATION.
+helpme () {
+	cat<<EOF
+	### DOTFILES ###
+	To edit dotfiles: use \`chezmoi\`.
+		edit: $ cm edit \$FILE
+		apply $ cm apply 
+	
+	Aliases are located at:
+		~/.zsh/custom/aliases.zsh
+
+	### HARVARD ###
+	hrc_sync
+		to sync with cluster
+		usage: $ hrc_sync \$SOURCE_FOLDER \$DESTINATION_FOLDER
+		
+		note that \$SOURCE_FOLDER should be a directory in \$mahadevan_lab/ntoyonaga (i.e. scratch).
+EOF
+}
